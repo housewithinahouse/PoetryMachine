f363e5fdfeff4749829e83db5dc47261f1b4002d
Edwin Fallwell <efallwel@gmail.com>
2018-04-09 12:39:43 -0400

    updates

--- poemprinter.py
+++ poemprinter.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python
 	 
 import os
-from time import sleep
-	 
+from time import sleep	 
 import RPi.GPIO as GPIO
 	 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
+GPIO.setup(23, GPIO.IN) #button 1
+GPIO.setup(24, GPIO.IN) #button 2
+
+GPIO.setup(17,GPIO.OUT) #LED for button 1
+GPIO.setup(27,GPIO.OUT) #LED for button 2
 
 os.system('sudo chown pi:pi /dev/usb/lp0')
-	 
+
 while True:
-    	if (GPIO.input(23) == False):
-   	 os.system('sh /home/pi/Desktop/PocketPoems/startup')
-   	 sleep(10);
+	GPIO.output(17, GPIO.HIGH)
+	GPIO.output(27, GPIO.HIGH)
+	
+	if (GPIO.input(23) == False):
+		os.system('sh /home/pi/Desktop/PocketPoems/startup')
+		GPIO.output(17, GPIO.LOW)
+		GPIO.output(27, GPIO.LOW)
+		sleep(10);
+		
+	if (GPIO.input(24) == False):
+		os.system('sh /home/pi/Desktop/PocketPoems/startup')
+		GPIO.output(17, GPIO.LOW)
+		GPIO.output(27, GPIO.LOW)
+		sleep(10);
